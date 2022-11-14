32c943623e173bef2742c58d08746c528e59857a
sravyakota <crazysravs577@gmail.com>
2022-11-14 08:15:02 +0000

    digital timer

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -3,24 +3,69 @@ import {Component} from 'react'
 import './index.css'
 
 class DigitalTimer extends Component {
-  state = {timer: 25, pauseStartButton: false}
+  state = {num: 25, pauseStartButton: false, timerInMins: 25, timerInSec: '00'}
 
   pauseStartChangingButton = () => {
     this.setState(prevState => ({
       pauseStartButton: !prevState.pauseStartButton,
     }))
   }
+  //   incrementTimeElapsedInSeconds = () => {
+  //     const {timerInMins, timerInSec} = this.state
+  //     const isTimerCompleted = timerInSec === timerInMins * 60
+
+  //     if (isTimerCompleted) {
+  //       this.clearTimerInterval()
+  //       this.setState({pauseStartButton: false})
+  //     } else {
+  //       this.setState(prevState => ({
+  //         timerInSec: prevState.timerInSec + 1,
+  //       }))
+  //     }
+  //   }
+
+  //   pauseStartChangingButton = () => {
+  //     const {pauseStartButton, timerInSec, timerInMins} = this.state
+  //     const sec = parseInt(timerInSec)
+
+  //     const isTimerCompleted = sec === timerInMins * 60
+
+  //     if (isTimerCompleted) {
+  //       this.setState({timerInSec: '00'})
+  //     }
+  //     if (pauseStartButton) {
+  //       this.clearTimerInterval()
+  //     } else {
+  //       this.intervalId = setInterval(this.incrementTimeElapsedInSeconds, 1000)
+  //     }
+  //     this.setState(prevState => ({
+  //       pauseStartButton: !prevState.pauseStartButton,
+  //     }))
+  //   }
 
   decrement = () => {
-    this.setState(prevState => ({timer: prevState.timer - 1}))
+    const {timerInMins} = this.state
+    this.setState(prevState => ({num: prevState.num - 1}))
+    const s = timerInMins - 1
+    this.setState({timerInMins: s})
   }
 
   increment = () => {
-    this.setState(prevState => ({timer: prevState.timer + 1}))
+    const {timerInMins} = this.state
+    this.setState(prevState => ({num: prevState.num + 1}))
+    const s = timerInMins + 1
+    this.setState({timerInMins: s})
+  }
+
+  resetButton = timeId => {
+    this.setState({timerInMins: 25})
+    this.setState({num: 25})
+    this.setState({timerInSec: '00'})
+    clearInterval(timeId)
   }
 
   render() {
-    const {timer, pauseStartButton} = this.state
+    const {num, pauseStartButton, timerInMins, timerInSec} = this.state
 
     const s = pauseStartButton ? 'Pause' : 'Start'
     const im = pauseStartButton
@@ -36,7 +81,9 @@ class DigitalTimer extends Component {
         <div className="digitalTimerContainer">
           <div className="digitalClockDisplayContainer">
             <div className="whiteBg">
-              <h1 className="timerDisplay">11:00</h1>
+              <h1 className="timerDisplay">
+                {timerInMins}:{timerInSec}
+              </h1>
               <p className="para">{sPara}</p>
             </div>
           </div>
@@ -58,7 +105,11 @@ class DigitalTimer extends Component {
                   alt="reset icon"
                   className="logos"
                 />
-                <button type="button" className="buttuns">
+                <button
+                  type="button"
+                  className="buttuns"
+                  onClick={this.resetButton}
+                >
                   Reset
                 </button>
               </div>
@@ -75,7 +126,7 @@ class DigitalTimer extends Component {
                 </button>
               </div>
               <div className="cc">
-                <p>{timer}</p>
+                <p>{num}</p>
               </div>
               <div>
                 <button
